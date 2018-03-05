a497d32e62b837f6aa3cf4df999de37d75a5de4c
Takayuki Sei <takayuki_sei@voyagegroup.com>
2018-03-05 16:11:52 +0900

    imple call java native plugin

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SceneController : MonoBehaviour {
+public class SceneController : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AndroidJavaObject jo = new AndroidJavaObject("daybysay.com.mylog.MyLog");
+        jo.Call("output", "unity test dayo");
+    }
 }
