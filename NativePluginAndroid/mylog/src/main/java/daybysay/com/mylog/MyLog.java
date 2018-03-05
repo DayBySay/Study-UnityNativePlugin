2c94b3d579d812d2f2b33e792e5572d92e3e1cfa
Takayuki Sei <takayuki_sei@voyagegroup.com>
2018-03-05 15:42:32 +0900

    add module for native lib

---
+++ NativePluginAndroid/mylog/src/main/java/daybysay/com/mylog/MyLog.java
@@ -0,0 +1,13 @@
+package daybysay.com.mylog;
+
+import android.util.Log;
+
+/**
+ * Created by t-sei on 2018/03/05.
+ */
+
+public class MyLog {
+    public void output(String text) {
+        Log.d("myLog", text);
+    }
+}
